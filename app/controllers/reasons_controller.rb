94661549a14d02853b472994da4fce0d943ef094
Faizan Anwar <faizana1298@gmail.com>
2024-01-06 19:05:16 +0530

    Added CRUD operation to the reason, added modal on new and edit button.

--- app/controllers/reasons_controller.rb
+++ app/controllers/reasons_controller.rb
@@ -1,6 +1,39 @@
 class ReasonsController < ApplicationController
 
-  before_action :set_reason, only: [:toggle_active]
+  before_action :set_storefront, only: [:new, :create]
+  before_action :set_reason, only: [:toggle_active, :edit, :update, :destroy]
+  
+  def new
+    @reason = @storefront.reasons.build
+  end
+
+  def create
+    @reason = @storefront.reasons.build(reason_params)
+
+    if @reason.save
+      render turbo_stream: [
+        turbo_stream.prepend("reasons_list", partial: "reasons/reason", locals: { reason: @reason, storefront: @storefront }),
+        turbo_stream.replace("reason_modal", "")
+      ]
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @storefront = @reason.storefront
+  end
+
+  def update
+    @storefront = @reason.storefront
+    if @reason.update(reason_params)
+      render turbo_stream: [
+        turbo_stream.replace(@reason, partial: 'reasons/reason', locals: { reason: @reason, storefront: @storefront })
+      ]
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   def toggle_active
     @storefront = @reason.storefront
@@ -9,6 +42,13 @@ class ReasonsController < ApplicationController
       format.turbo_stream
       format.html { redirect_back(fallback_location: root_path) }
     end
+  end
+
+  def destroy
+    @reason.destroy
+    render turbo_stream: [
+      turbo_stream.remove(@reason)
+    ]
   end
 
   def update_order
@@ -30,8 +70,16 @@ class ReasonsController < ApplicationController
     @reason = Reason.find(params[:id])
   end
 
+  def set_storefront
+    @storefront = Storefront.find(params[:storefront_id])
+  end
+
   def reason_order_params
     params.permit(reasons: [:id, :ordering])
+  end
+
+  def reason_params
+    params.require(:reason).permit(:label, :code, :active, :storefront_id)
   end
 
 end
--- app/models/reason.rb
+++ app/models/reason.rb
@@ -5,4 +5,13 @@ class Reason < ApplicationRecord
   validates :label, presence: true
 
   scope :ordered, -> { order(ordering: :asc) }
+
+  after_create :set_ordering
+
+  private
+
+  def set_ordering
+    max_ordering = self.storefront.reasons.maximum(:ordering) || 0
+    update_column(:ordering, max_ordering + 1)
+  end
 end
